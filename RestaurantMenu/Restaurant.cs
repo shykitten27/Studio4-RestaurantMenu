80f9454b6cef3b053e4bce2b0acd9b2746d3c784
Donna Seidel <dseidel01@gmail.com>
2021-08-03 02:44:05 -0500

    call method in Restuarant class.

--- RestaurantMenu/Restaurant.cs
+++ RestaurantMenu/Restaurant.cs
@@ -9,15 +9,18 @@ namespace RestaurantMenu
         static void Main(string[] args)
         {
             //instantiate new MenuItem
-            MenuItem item1 = new MenuItem("Tiramisu", "Ladyfingers dipped in coffee, layered with marscapone filling and dusted with cocoa.", 6.00, categoryList, false);
-            MenuItem item2 = new MenuItem("Aviation", "Creme de violette blended with gin, lemon juice and a brandied cherry.", 8.00, categoryList, false);
-            MenuItem item3 = new MenuItem("Shrimp Fra Diavolo", "Fiery spiced shrimp over a bed of angel hair pasta.", 18.00, categoryList, true); //new item boolean set
+            MenuItem item1 = new MenuItem("Tiramisu", "Ladyfingers dipped in coffee, layered with marscapone filling and dusted with cocoa.", 6.00, "Dessert", false);
+            MenuItem item2 = new MenuItem("Aviation", "Creme de violette blended with gin, lemon juice and a brandied cherry.", 8.00, "Cocktail", false);
+            MenuItem item3 = new MenuItem("Shrimp Fra Diavolo", "Fiery spiced shrimp over a bed of angel hair pasta.", 18.00, "Entrée", true); //new item boolean set
+            MenuItem item4 = new MenuItem("Zucchini Fritté", "Crispy, breaded, oven-fried zucchini served with aioli  dipping sauce.", 5.95, "Appetizer", true);
 
             //instatian a new Menu
             Menu currentMenu = new Menu();
 
             Menu.PrintMenu();
             Menu.PrintMenu("Aviation");
+            Menu.UpdateMenu("remove", "Aviation"); //hmmm...maybe the add should be an overloaded method and the remove only uses item name
+            Menu.PrintMenu();
         }
     }
 }
