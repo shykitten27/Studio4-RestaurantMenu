0dbe886f6fa2ce6d7f32ae99423cb9b119f20ff2
Donna Seidel <dseidel01@gmail.com>
2021-08-03 01:45:52 -0500

    Implement UpdateMenu method

--- RestaurantMenu/Menu.cs
+++ RestaurantMenu/Menu.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace RestaurantMenu
@@ -14,6 +15,41 @@ namespace RestaurantMenu
             MenuItems = menuItems;
             LastUpdated = DateTime.Now;
         }
-    }
 
+        private void UpdateMenu(string action, string name, string description, double price, List<string> category, bool isNew)
+        {
+            //check for valid actions
+            if (!action.Equals("add") || !action.Equals("remove")) {
+                Console.WriteLine("Invalid action - must be add or remove.");
+                return;
+            }
+            else if (action.Equals("add")) //ADD 
+            {
+                if (MenuItems.Contains(name)) //check for existence 
+                { 
+                    Console.WriteLine("Menu item already exists."); 
+                    return;
+                }
+                else //does not already exist add new MenuItem.
+                {
+                    MenuItem newItem = new MenuItem(name, description, price, category, true);
+                    Console.WriteLine(name + " added to menu.");
+                    return;
+                }
+            }
+            else if (action.Equals("remove")) //REMOVE 
+            {
+                if (MenuItems.Contains(name)) //check for existence before removing
+                {
+                    Console.WriteLine("Menu item " + name + " removed.");
+                    return;
+                }
+                else //does not exist, display warning.
+                {
+                    Console.WriteLine("Menu item " + name + " does not exist.");
+                    return;
+                }
+            }
+        }
+    }
 }
